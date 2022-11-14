852177c3a109169921de49b8ae696523a48b84b1
Thomas Ralls <tralls.pnw@gmail.com>
2022-11-13 22:11:40 -0800

    Start nav style

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,10 @@
-import { Banner, BannerList } from "./banner.js";
+import { BannerList } from "./banner.js";
+import { Nav } from "./nav.js";
 
 class App {
     run() {
+        const nav = new Nav();
+        nav.start();
         const banners = new BannerList();
         banners.startAll();
     }
