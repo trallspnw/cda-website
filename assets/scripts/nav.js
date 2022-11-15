70889d31d1d44accd3ff92b10f4f49a46b5cec6a
Thomas Ralls <tralls.pnw@gmail.com>
2022-11-14 22:29:34 -0800

    Improve nav

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,18 +1,11 @@
 export class Nav {
-    start() {
-        const navbarToggle = navbar.querySelector("#navbar-toggle");
-        const navbarMenu = document.querySelector("#navbar-menu");
-        const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
-        let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
-        
-        const toggleNavbarVisibility = () => {
-          isNavbarExpanded = !isNavbarExpanded;
-          navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
-        };
-        
-        navbarToggle.addEventListener("click", toggleNavbarVisibility);
-        
-        navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
-        navbarMenu.addEventListener("click", toggleNavbarVisibility);
+    async start() {
+        $('.navbar-item').hover(
+            function () {
+                $(this).find('.navbar-sub-nav').show();
+            },
+            function () {
+                $(this).find('.navbar-sub-nav').hide();
+            });
     }
 }
