d4d99b1202a930a3ea3cc77d994f17c5d8ffae61
Thomas Ralls <tralls.pnw@gmail.com>
2022-11-22 21:16:17 -0800

    Responsive nav

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,11 +1,31 @@
 export class Nav {
     async start() {
-        $('.nav-item').hover(
+        this.stackNav();
+        $('.nav-container .nav-item').hover(
             function () {
                 $(this).find('.nav-sub-items').show();
             },
             function () {
                 $(this).find('.nav-sub-items').hide();
             });
+
+        $(window).on('resize', () => {
+            this.stackNav();
+        });  
+    }
+
+    stackNav() {
+        const navMenuWidth = $('.nav-menu').width();
+        let spaceUsed = 0;
+        $('.nav-container .nav-item').each(function(index) {
+            $(this).removeClass('in-nav in-hamburger');
+            spaceUsed += this.offsetWidth;
+            if (spaceUsed <= navMenuWidth) {
+                $(this).addClass('in-nav');
+            } else {
+                $(this).addClass('in-hamburger');
+                // sub menu
+            }
+        })
     }
 }
